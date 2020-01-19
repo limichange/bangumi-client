8674d2cab8ca5d3c9e399c2ef9d4686c8fa80534
limichange <limichange@hotmail.com>
2020-01-19 13:23:28 +0800

    Update

---
+++ lib/pages/AnimeDetailPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AnimeDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text("半谷米"),
+        ),
+        body: Column(children: <Widget>[
+          Container(),
+          Container(),
+        ]));
+  }
+}
