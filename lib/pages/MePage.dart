71631128b24ffe23537eb324585472436c34941d
limichange <limichange@hotmail.com>
2020-01-21 18:57:32 +0800

    Update

--- lib/pages/MePage.dart
+++ lib/pages/MePage.dart
@@ -1,7 +1,33 @@
 import 'package:bangumi/pages/LoginAndSignupPage/LoginAndSignupPage.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class MePage extends StatelessWidget {
+class MePage extends StatefulWidget {
+  @override
+  _MePage createState() {
+    return _MePage();
+  }
+}
+
+class _MePage extends State<MePage> {
+  String key;
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadKey();
+  }
+
+  loadKey() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      prefs.setString('key', 'value');
+      key = prefs.getString('key');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,22 +36,28 @@ class MePage extends StatelessWidget {
         // the App.build method, and use it to set our appbar title.
         title: Text("我的"),
       ),
-      body: (Container(
-        alignment: Alignment.center,
-        child: RaisedButton(
-          child: Text(
-            '登录/注册',
-          ),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => LoginAndSignupPage()),
-            );
-          },
-          color: Colors.pink[300],
-          textColor: Colors.white,
-        ),
-      )),
+      body: Column(
+        children: <Widget>[
+          Text(key),
+          (Container(
+            padding: EdgeInsets.all(30.0),
+            alignment: Alignment.center,
+            child: RaisedButton(
+              child: Text(
+                '登录/注册',
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginAndSignupPage()),
+                );
+              },
+              color: Colors.pink[300],
+              textColor: Colors.white,
+            ),
+          ))
+        ],
+      ),
     );
   }
 }
