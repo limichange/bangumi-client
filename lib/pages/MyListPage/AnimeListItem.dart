3d99ba5a8b865c35ab5504c568cedd7677dd6147
limichange <limichange@hotmail.com>
2020-01-21 18:24:47 +0800

    Update

---
+++ lib/pages/MyListPage/AnimeListItem.dart
@@ -0,0 +1,57 @@
+import 'package:bangumi/model/Anime.dart';
+import 'package:bangumi/pages/AnimeDetailPage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AnimeListItem extends StatelessWidget {
+  Anime anime;
+
+  AnimeListItem({this.anime});
+
+  goDetail(context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => AnimeDetailPage(uuid: anime.uuid)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 10, top: 10, right: 10),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            width: 60,
+            child: Image.network(anime.cover),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.only(left: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      anime.name,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.start,
+                      maxLines: 1,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/MyListPage/MyListPage.dart
+++ lib/pages/MyListPage/MyListPage.dart
@@ -1,6 +1,23 @@
+import 'package:bangumi/api/API.dart';
+import 'package:bangumi/pages/MyListPage//AnimeListItem.dart';
+import 'package:bangumi/model/Anime.dart';
 import 'package:flutter/material.dart';
 
-class MyListPage extends StatelessWidget {
+class MyListPage extends StatefulWidget {
+  @override
+  _MyListPage createState() => _MyListPage();
+}
+
+class _MyListPage extends State<MyListPage> {
+  Future<List<Anime>> data;
+
+  @override
+  void initState() {
+    super.initState();
+
+    data = API.getAnimeHome();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,14 +26,28 @@ class MyListPage extends StatelessWidget {
           // the App.build method, and use it to set our appbar title.
           title: Text("我的收藏"),
         ),
-        body: (Container(
-          child: Column(
-            children: <Widget>[
-              Text(
-                '我的收藏2',
-              ),
-            ],
-          ),
-        )));
+        body: Column(children: <Widget>[
+          Flexible(
+            flex: 1,
+            child: RefreshIndicator(
+                onRefresh: () {},
+                child: FutureBuilder<List<Anime>>(
+                    future: data,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return ListView.builder(
+                          itemCount: snapshot.data.length,
+                          itemBuilder: (context, int index) {
+                            return AnimeListItem(anime: snapshot.data[index]);
+                          },
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text('error');
+                      } else {
+                        return CircularProgressIndicator();
+                      }
+                    })),
+          )
+        ]));
   }
 }
