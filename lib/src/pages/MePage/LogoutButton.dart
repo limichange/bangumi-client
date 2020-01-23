3d38eae16940f723328d96cc56f913a2f2a562fc
limichange <limichange@hotmail.com>
2020-01-23 22:34:17 +0800

    Update

--- lib/src/pages/MePage/LogoutButton.dart
+++ lib/src/pages/MePage/LogoutButton.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LogoutButton extends StatelessWidget {
+  signout() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('token');
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
