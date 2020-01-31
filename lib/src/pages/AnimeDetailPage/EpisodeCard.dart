c7a81318f63411b8006d569a363bbacf28775727
limichange <limichange@hotmail.com>
2020-01-31 22:29:03 +0800

    Update

--- lib/src/pages/AnimeDetailPage/EpisodeCard.dart
+++ lib/src/pages/AnimeDetailPage/EpisodeCard.dart
@@ -12,48 +12,50 @@ class EpisodeCard extends StatelessWidget {
     var url = episode.biliUrl;
 
     if (await canLaunch(url)) {
-      await launch(url);
+      await launch(url, forceSafariVC: false);
     } else {
-//      todo
+//      todoc
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: new EdgeInsets.only(right: 10),
-        padding: EdgeInsets.all(10),
-        decoration: new BoxDecoration(
-          border: new Border.all(
-              color: Colors.black12, width: 1, style: BorderStyle.solid),
-        ),
-        width: 200,
-        height: 85,
-        child: GestureDetector(
+      child: GestureDetector(
           onTap: onTap,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                '第' + episode.title + '话',
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              Container(
-                margin: new EdgeInsets.only(top: 5),
-                child: Text(
-                  episode.longTitle,
+          child: Container(
+            margin: new EdgeInsets.only(right: 10),
+            padding: EdgeInsets.all(10),
+            decoration: new BoxDecoration(
+              border: new Border.all(
+                  color: Colors.black12, width: 1, style: BorderStyle.solid),
+            ),
+            width: 200,
+            height: 90,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  episode.title == null ? ' ' : '第' + episode.title + '话',
                   style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.black38,
-                    fontWeight: FontWeight.w400,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
-              )
-            ],
-          ),
-        ));
+                Container(
+                  margin: new EdgeInsets.only(top: 5),
+                  child: Text(
+                    episode.longTitle == null ? ' ' : episode.longTitle,
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.black38,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
   }
 }
