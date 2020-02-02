bbc23468137c6181d94a5c621fb2d8a50f6b82d9
limichange <limichange@hotmail.com>
2020-02-02 11:43:10 +0800

    Update

---
+++ lib/src/pages/FeedbackPage/FeedbackPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FeedbackPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _FeedbackPage();
+  }
+}
+
+class _FeedbackPage extends State<FeedbackPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
